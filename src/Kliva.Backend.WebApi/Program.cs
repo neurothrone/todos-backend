7420a68512adb1d6eefda6f90b29869546e2ea42
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-04-26 09:21:07 +0200

    remove firebase auth endpoints and related services from the backend

--- src/Kliva.Backend.WebApi/Program.cs
+++ src/Kliva.Backend.WebApi/Program.cs
@@ -1,43 +1,7 @@
-using Kliva.Backend.WebApi.Endpoints;
-using Kliva.Backend.WebApi.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddAntiforgery();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "Kliva.Auth";
-        options.Cookie.HttpOnly = true;
-        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-        options.Cookie.SameSite = SameSiteMode.Strict;
-        options.ExpireTimeSpan = TimeSpan.FromDays(7);
-        options.LoginPath = "/login";
-        options.LogoutPath = "/";
-        options.AccessDeniedPath = "/access-denied";
-    });
-builder.Services.AddAuthorization();
-
-// Configure Firebase
-var firebaseConfig = builder.Configuration.GetSection("Firebase");
-var apiKey = firebaseConfig["ApiKey"];
-var authDomain = firebaseConfig["AuthDomain"];
-
-if (string.IsNullOrEmpty(apiKey))
-    throw new Exception("Firebase API key is not configured");
-if (string.IsNullOrEmpty(authDomain))
-    throw new Exception("Firebase Auth Domain is not configured");
-
-builder.Services.AddScoped(provider => new FirebaseAuthService(
-    provider.GetRequiredService<ILogger<FirebaseAuthService>>(),
-    provider.GetRequiredService<IConfiguration>(),
-    provider.GetRequiredService<IHttpContextAccessor>()));
 
 var app = builder.Build();
 
@@ -49,10 +13,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseAntiforgery();
 
 app.UseStaticFiles();
 app.MapGet("/", context =>
@@ -60,7 +22,5 @@ app.MapGet("/", context =>
     context.Response.Redirect("/index.html");
     return Task.CompletedTask;
 });
-
-app.MapAuthEndpoints();
 
 app.Run();
